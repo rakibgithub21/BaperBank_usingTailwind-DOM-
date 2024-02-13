ea512cd0c053cdb72e23e8c501fd19efbb520141
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-02-13 15:15:12 +0600

    withdraw validation added

--- scripts/withdraw.js
+++ scripts/withdraw.js
@@ -2,6 +2,12 @@ document.getElementById('withdraw-btn').addEventListener('click', function () {
     const withdrawField = document.getElementById('withdraw-field');
     const newWithdrawAmountString = withdrawField.value;
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+    withdrawField.value = '';
+
+    if (isNaN(newWithdrawAmount)) {
+        alert('please provide a valid number')
+        return;
+    }
 
 
     const withdrawElement = document.getElementById('withdraw-total');
@@ -9,14 +15,21 @@ document.getElementById('withdraw-btn').addEventListener('click', function () {
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
 
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    withdrawElement.innerText = currentWithdrawTotal;
+    
 
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+    
+
+    if (newWithdrawAmount > previousBalanceTotal) {
+        alert('Baaper Bank e eto taka nai');
+        return;
+    }
+
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawElement.innerText = currentWithdrawTotal;
 
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
     balanceTotalElement.innerText = newBalanceTotal;
-    withdrawField.value = '';
 })
